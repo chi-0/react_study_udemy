17dee491ad5ebf9827ff01cfe85b415160f9981e
chi-0 <sc0o0@naver.com>
2023-11-02 21:04:22 +0900

    Expense app end

--- basic01/src/components/Expenses/Expenses.js
+++ basic01/src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import { ExpensesFilter } from "./ExpensesFilter";
 import "./Expenses.css";
 import { useState } from "react";
 import { ExpensesList } from "./ExpensesList";
+import { ExpensesChart } from "./ExpensesChart";
 
 export const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -22,6 +23,7 @@ export const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
