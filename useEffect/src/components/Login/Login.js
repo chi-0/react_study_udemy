a30c2080593d5c2c1ef23f179e21aceb78175b40
chi-0 <sc0o0@naver.com>
2023-11-14 21:53:52 +0900

    useEffect 클린업함수

--- useEffect/src/components/Login/Login.js
+++ useEffect/src/components/Login/Login.js
@@ -12,9 +12,16 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    setTimeout(() => {
+      console.log("hihihi");
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log("clean up");
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
